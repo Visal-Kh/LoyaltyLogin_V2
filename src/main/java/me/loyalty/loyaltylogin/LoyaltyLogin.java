414183daf0584fa4745dd6de79f531d28a566e6f
BroVisal <gfjhffb3@gmail.com>
2026-07-09 23:44:40 +0700

    Implement LoyaltyLogin plugin structure
    Added basic plugin structure with onEnable and onDisable methods.

--- src/main/java/me/loyalty/loyaltylogin/LoyaltyLogin.java
+++ src/main/java/me/loyalty/loyaltylogin/LoyaltyLogin.java
@@ -1,1 +1,27 @@
+package me.loyalty.loyaltylogin;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LoyaltyLogin extends JavaPlugin {
+
+    private static LoyaltyLogin instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        getLogger().info("=================================");
+        getLogger().info(" LoyaltyLogin v2.0 Enabled!");
+        getLogger().info(" Author: LoyaltyMC");
+        getLogger().info("=================================");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("LoyaltyLogin Disabled!");
+    }
+
+    public static LoyaltyLogin getInstance() {
+        return instance;
+    }
+}
