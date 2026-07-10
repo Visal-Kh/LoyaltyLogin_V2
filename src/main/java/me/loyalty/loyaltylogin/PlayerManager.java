04853d00933e483d1d7c42d6cff511afcc87d3aa
BroVisal <gfjhffb3@gmail.com>
2026-07-10 19:04:00 +0700

    Refactor PlayerManager to use UUID for login methods

--- src/main/java/me/loyalty/loyaltylogin/PlayerManager.java
+++ src/main/java/me/loyalty/loyaltylogin/PlayerManager.java
@@ -1,7 +1,5 @@
 package me.loyalty.loyaltylogin;
 
-import org.bukkit.entity.Player;
-
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -10,15 +8,15 @@ public class PlayerManager {
 
     private final Set<UUID> loggedInPlayers = new HashSet<>();
 
-    public boolean isLoggedIn(Player player) {
-        return loggedInPlayers.contains(player.getUniqueId());
+    public void login(UUID uuid) {
+        loggedInPlayers.add(uuid);
     }
 
-    public void login(Player player) {
-        loggedInPlayers.add(player.getUniqueId());
+    public void logout(UUID uuid) {
+        loggedInPlayers.remove(uuid);
     }
 
-    public void logout(Player player) {
-        loggedInPlayers.remove(player.getUniqueId());
+    public boolean isLoggedIn(UUID uuid) {
+        return loggedInPlayers.contains(uuid);
     }
 }
