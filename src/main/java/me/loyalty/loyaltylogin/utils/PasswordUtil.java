20cadcf70282ade8a0b90826379a9a8bd676b51a
BroVisal <gfjhffb3@gmail.com>
2026-07-10 21:03:15 +0700

    Add PasswordUtil for hashing and checking passwords

--- src/main/java/me/loyalty/loyaltylogin/utils/PasswordUtil.java
+++ src/main/java/me/loyalty/loyaltylogin/utils/PasswordUtil.java
@@ -1,1 +1,14 @@
+package me.loyalty.loyaltylogin.utils;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String password, String hashedPassword) {
+        return BCrypt.checkpw(password, hashedPassword);
+    }
+}
