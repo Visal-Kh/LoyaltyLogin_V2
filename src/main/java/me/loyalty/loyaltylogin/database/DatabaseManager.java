3d5d6a0a4d3f9c79bd8388c31a9e5dc54cf0ce0c
BroVisal <gfjhffb3@gmail.com>
2026-07-10 08:19:29 +0700

    Implement DatabaseManager for SQLite connection

--- src/main/java/me/loyalty/loyaltylogin/database/DatabaseManager.java
+++ src/main/java/me/loyalty/loyaltylogin/database/DatabaseManager.java
@@ -1,1 +1,29 @@
+package me.loyalty.loyaltylogin.database;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import me.loyalty.loyaltylogin.LoyaltyLogin;
+
+public class DatabaseManager {
+
+    private Connection connection;
+
+    public void connect() {
+        try {
+            File file = new File(LoyaltyLogin.getInstance().getDataFolder(), "database.db");
+
+            connection = DriverManager.getConnection("jdbc:sqlite:" + file);
+
+            LoyaltyLogin.getInstance().getLogger().info("SQLite Connected!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
