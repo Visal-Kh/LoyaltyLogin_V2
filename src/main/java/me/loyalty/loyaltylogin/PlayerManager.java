9be148351a3d67984927aa513d80b4e55af1f4df
BroVisal <gfjhffb3@gmail.com>
2026-07-10 08:16:04 +0700

    Implement PlayerManager for login tracking

--- src/main/java/me/loyalty/loyaltylogin/PlayerManager.java
+++ src/main/java/me/loyalty/loyaltylogin/PlayerManager.java
@@ -1,1 +1,24 @@
+package me.loyalty.loyaltylogin;
 
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class PlayerManager {
+
+    private final Set<UUID> loggedInPlayers = new HashSet<>();
+
+    public boolean isLoggedIn(Player player) {
+        return loggedInPlayers.contains(player.getUniqueId());
+    }
+
+    public void login(Player player) {
+        loggedInPlayers.add(player.getUniqueId());
+    }
+
+    public void logout(Player player) {
+        loggedInPlayers.remove(player.getUniqueId());
+    }
+}
