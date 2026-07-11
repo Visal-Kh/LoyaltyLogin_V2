99327cb5a721a6269a89548f2a01727953ce15d7
BroVisal <gfjhffb3@gmail.com>
2026-07-11 08:04:32 +0700

    Add LoginManager to LoyaltyLogin plugin

--- src/main/java/me/loyalty/loyaltylogin/LoyaltyLogin.java
+++ src/main/java/me/loyalty/loyaltylogin/LoyaltyLogin.java
@@ -1,39 +1,72 @@
 package me.loyalty.loyaltylogin;
+
 import me.loyalty.loyaltylogin.database.DatabaseManager;
+import me.loyalty.loyaltylogin.login.LoginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LoyaltyLogin extends JavaPlugin {
 
     private static LoyaltyLogin instance;
+
     private PlayerManager playerManager;
     private DatabaseManager databaseManager;
+    private LoginManager loginManager;
+
 
     @Override
     public void onEnable() {
+
         instance = this;
 
         saveDefaultConfig();
 
+
         playerManager = new PlayerManager();
+
         databaseManager = new DatabaseManager();
         databaseManager.connect();
         databaseManager.createTables();
+
+
+        loginManager = new LoginManager();
+
+
         getLogger().info("LoyaltyLogin v2 Enabled!");
     }
 
+
     @Override
     public void onDisable() {
+
+        if(databaseManager != null) {
+            databaseManager.disconnect();
+        }
+
+        if(loginManager != null) {
+            loginManager.clear();
+        }
+
+
         getLogger().info("LoyaltyLogin Disabled!");
     }
+
 
     public static LoyaltyLogin getInstance() {
         return instance;
     }
 
+
     public PlayerManager getPlayerManager() {
         return playerManager;
     }
+
+
     public DatabaseManager getDatabaseManager() {
-    return databaseManager;
+        return databaseManager;
+    }
+
+
+    public LoginManager getLoginManager() {
+        return loginManager;
     }
 }
