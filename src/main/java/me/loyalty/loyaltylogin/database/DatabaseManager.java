7cd90faeae3deeb51ede015a275a28d96516123a
BroVisal <gfjhffb3@gmail.com>
2026-07-11 08:19:27 +0700

    Update DatabaseManager to include premium field

--- src/main/java/me/loyalty/loyaltylogin/database/DatabaseManager.java
+++ src/main/java/me/loyalty/loyaltylogin/database/DatabaseManager.java
@@ -21,9 +21,11 @@ public class DatabaseManager {
                     "database.db"
             );
 
+
             if (!file.getParentFile().exists()) {
                 file.getParentFile().mkdirs();
             }
+
 
             connection = DriverManager.getConnection(
                     "jdbc:sqlite:" + file
@@ -34,15 +36,22 @@ public class DatabaseManager {
                     .getLogger()
                     .info("SQLite Connected!");
 
+
         } catch (SQLException e) {
+
             e.printStackTrace();
+
         }
     }
 
 
+
     public Connection getConnection() {
+
         return connection;
+
     }
+
 
 
     public void createTables() {
@@ -51,44 +60,69 @@ public class DatabaseManager {
 
             connection.createStatement()
                     .executeUpdate(
+
                     "CREATE TABLE IF NOT EXISTS players (" +
+
                     "uuid TEXT PRIMARY KEY," +
+
                     "name TEXT NOT NULL," +
+
                     "password TEXT NOT NULL," +
+
                     "premium INTEGER DEFAULT 0" +
+
                     ");"
+
             );
 
 
         } catch (SQLException e) {
+
             e.printStackTrace();
+
         }
     }
+
 
 
     public boolean isRegistered(String uuid) {
 
+
         try {
 
+
             var ps = connection.prepareStatement(
+
                     "SELECT uuid FROM players WHERE uuid=?"
+
             );
+
 
             ps.setString(1, uuid);
 
+
             var rs = ps.executeQuery();
+
 
             return rs.next();
 
 
+
         } catch (SQLException e) {
+
 
             e.printStackTrace();
 
+
         }
 
+
         return false;
+
     }
+
+
+
 
 
     public void registerPlayer(
@@ -97,41 +131,133 @@ public class DatabaseManager {
             String password
     ) {
 
+
         try {
 
+
             var ps = connection.prepareStatement(
-                    "INSERT INTO players(uuid,name,password) VALUES(?,?,?)"
+
+                    "INSERT INTO players(uuid,name,password,premium) VALUES(?,?,?,?)"
+
             );
 
 
             ps.setString(1, uuid);
+
             ps.setString(2, name);
+
             ps.setString(3, password);
+
+            ps.setInt(4, 0);
+
 
 
             ps.executeUpdate();
 
 
+
         } catch (SQLException e) {
+
 
             e.printStackTrace();
 
+
         }
+
     }
+
+
+
+
+
+
+    public boolean checkPassword(
+            String uuid,
+            String password
+    ) {
+
+
+        try {
+
+
+            var ps = connection.prepareStatement(
+
+                    "SELECT password FROM players WHERE uuid=?"
+
+            );
+
+
+            ps.setString(1, uuid);
+
+
+
+            var rs = ps.executeQuery();
+
+
+
+            if (rs.next()) {
+
+
+                String savedPassword =
+                        rs.getString("password");
+
+
+
+                return savedPassword.equals(password);
+
+
+            }
+
+
+
+        } catch (SQLException e) {
+
+
+            e.printStackTrace();
+
+
+        }
+
+
+
+        return false;
+
+    }
+
+
+
+
 
 
     public void disconnect() {
 
+
         try {
 
-            if(connection != null) {
+
+            if (connection != null &&
+                    !connection.isClosed()) {
+
+
                 connection.close();
+
+
+                LoyaltyLogin.getInstance()
+                        .getLogger()
+                        .info("SQLite Disconnected!");
+
             }
 
-        } catch(SQLException e) {
+
+
+        } catch (SQLException e) {
+
 
             e.printStackTrace();
 
+
         }
+
     }
+
 }
