5603ba15a81eca73fe594ef627af28112b6cade6
BroVisal <gfjhffb3@gmail.com>
2026-07-11 08:55:30 +0700

    Implement PremiumManager for premium user management

--- src/main/java/me/loyalty/loyaltylogin/premium/PremiumManager.java
+++ src/main/java/me/loyalty/loyaltylogin/premium/PremiumManager.java
@@ -1,1 +1,34 @@
+package me.loyalty.loyaltylogin.premium;
 
+import me.loyalty.loyaltylogin.database.DatabaseManager;
+
+import java.util.UUID;
+
+public class PremiumManager {
+
+    private final DatabaseManager database;
+
+
+    public PremiumManager(DatabaseManager database) {
+        this.database = database;
+    }
+
+
+    public boolean isPremium(UUID uuid) {
+
+        return database.isPremium(
+                uuid.toString()
+        );
+
+    }
+
+
+    public void setPremium(UUID uuid) {
+
+        database.setPremium(
+                uuid.toString()
+        );
+
+    }
+
+}
