a294cc024f6beb3b8a5d0f88103dfd83cb56cb45
BroVisal <gfjhffb3@gmail.com>
2026-07-11 08:29:47 +0700

    Implement password hashing and verification methods

--- src/main/java/me/loyalty/loyaltylogin/security/PasswordManager.java
+++ src/main/java/me/loyalty/loyaltylogin/security/PasswordManager.java
@@ -1,1 +1,55 @@
+package me.loyalty.loyaltylogin.security;
 
+import java.security.MessageDigest;
+
+public class PasswordManager {
+
+
+    public static String hash(String password) {
+
+        try {
+
+            MessageDigest md =
+                    MessageDigest.getInstance("SHA-256");
+
+
+            byte[] hash =
+                    md.digest(password.getBytes());
+
+
+            StringBuilder hex =
+                    new StringBuilder();
+
+
+            for(byte b : hash) {
+
+                hex.append(
+                        String.format("%02x", b)
+                );
+
+            }
+
+
+            return hex.toString();
+
+
+        } catch (Exception e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+
+    public static boolean verify(
+            String password,
+            String hashedPassword
+    ) {
+
+        return hash(password)
+                .equals(hashedPassword);
+
+    }
+
+}
